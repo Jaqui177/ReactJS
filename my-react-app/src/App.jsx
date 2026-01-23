6b911f2364a3868e614eeecc7572c6470668b118
Jaquelin Soto <240743@utxicotepec.edu.mx>
2026-01-23 14:32:14 -0600

    Se modifico el App.jxs de assets y tambuien se creo oto folder que se llama Encabezado.jsx y el Encabezado.css y tambien se agregaron 6 imagenes

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,5 +1,71 @@
-function App() {
-  return <h1>5A EVND</h1>
+import Encabezado from "./Encabezado";
+
+function App(){
+  return (
+  <div>
+    <Encabezado />
+    <h1>5A - EVND</h1>
+        <h2>Alumno</h2>
+        <h3>Jaquelin</h3>
+        <UserComponent/>
+        <ProfileComponent/>
+        <FeedComponent/>
+        </div>
+  );
 }
- 
-export default App;
+
+function UserComponent(){
+  const nombre = 'Jaquelin';
+  const apellido = 'Soto Romero';
+  const nombreCompleto = <h3>Ella se llama: {nombre} y sus apellidos son {apellido}</h3>;
+  return <h3>UserComponent {nombreCompleto}</h3>;
+}
+
+function ProfileComponent(){
+   const users = [
+    { id: 1, name: 'Gustavo', role: 'Web Developer' },
+    { id: 2, name: 'Carlos', role: 'Web Designer' },
+    { id: 3, name: 'David', role: 'Team Leader' },
+  ]
+  return (
+    <>
+      <p>Lista de usuarios del Sistema</p>
+      <ul>
+        {
+        users.map(function(user,index){
+          return(
+            <li key={index}>{user.name} Es un {user.role}</li>
+          )
+        })
+      }
+      </ul>
+      </>
+  );
+}
+
+function FeedComponent() {
+  const materiales = [
+    { id: 4, name: 'Cemento', tipo: 'Material Base' },
+    { id: 5, name: 'Ladrillo', tipo: 'Bloque de Construcción' },
+    { id: 6, name: 'clavo', tipo: 'Unir' },
+    { id: 7, name: 'Madera', tipo: 'Estructura' },
+    { id: 8, name: 'Martillo', tipo: 'Pegar' },
+  ]
+  return (
+    <>
+      <p><h2>Lista de Materiales de Construcción</h2></p>
+      <ul>
+        {
+        materiales.map(function(material,index){
+          return(
+            <li key={index}>{material.name} Es un {material.tipo}</li>
+          )
+        })
+      }
+      </ul>
+      </>
+  );
+}
+
+export default App
+
---
+++ my-react-app/src/Encabezado.jsx
@@ -0,0 +1,57 @@
+import miLogo from './assets/images.png';
+import facebook from './assets/facebook.png';
+import instagram from './assets/instagram.png';
+import tiktok from './assets/tiktok.png';
+import whatsapp from './assets/whatsapp.png';
+import youtube from './assets/youtube.png';
+import'./Encabezado.css';
+
+function Encabezado () {
+    return (
+        <div className= "Encabezado">
+            <Logo />
+            <Menu />
+            <Redes />
+           <h2>Bienvenido a mi sitio</h2>
+        </div>
+    );
+}
+
+function Logo(){
+    return (
+       <div className="Logo">
+        <img src={miLogo} alt="React Logo" />
+       </div>
+    )
+}
+
+
+function Menu(){
+    return (
+        <nav>
+            <ul>
+                <li>Inicio</li>
+                <li>Acerca de</li>
+                <li>Productos</li>
+                <li>Contacto</li>
+                <li>Sucursales</li>
+            </ul>
+        </nav>
+    )
+}
+
+function Redes(){
+    return (
+        <nav>
+            <ul>
+                <li><img src={facebook} alt="React facebook" /></li>
+                <li><img src={instagram} alt="React instagram" /></li>
+                <li><img src={tiktok} alt="React tiktok" /></li>
+                <li><img src={whatsapp} alt="React whatsapp" /></li>
+                <li><img src={youtube} alt="React youtube" /></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Encabezado;
